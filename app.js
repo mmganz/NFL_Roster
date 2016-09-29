63916fba232097a19e3ba72f9b125f49d15d9494
Meredith Ganz <2meredithganz@gmail.com>
2016-09-29 16:39:34 -0600

    push

---
+++ app.js
@@ -0,0 +1,57 @@
+
+
+function NewPlayer(name,position,jersey){
+    this.name = name;
+    this.position = position;
+    this.jersey = jersey;
+}
+
+var players = [
+    {name: 'Player Name',
+    position: 'Player Position',
+    jersey: 'Player Number'},
+]
+
+$("form").submit(function(event){
+     event.preventDefault();
+     var form = this;
+    
+    var p = new NewPlayer(
+        form.inputName.value,
+        form.inputPosition.value,
+        form.inputNumber.value
+    )
+
+    console.log(p)
+
+players.push(p);
+form.reset();
+cards(players);
+});
+
+function cards(players){
+    var elem = document.getElementById('player-roster')
+    elem.innerHTML = ''
+    var template = ''
+    for(var i=0; i<players.length; i++){
+        var nfl= players[i]
+        template+= `<div class="player-card">
+        <div class="card">
+         <img class="card-img-top" src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/">
+         <div class="card-block">
+          <p class="card-text cName text">${nfl.name}</p>
+         <p class="card-text cPosition text">${nfl.position}</p>
+         <p class="card-text cNumber text">${nfl.number}</p>
+         </div>
+         </div>
+         </div>`
+    }
+
+    elem.innerHTML = template;
+}
+
+cards(players);
+
+
+
+
