5c7115e50f4f5ff35478bcea81f95d9849b4c316
Meredith Ganz <2meredithganz@gmail.com>
2016-09-30 16:15:36 -0600

    push

--- app.js
+++ app.js
@@ -1,57 +1,97 @@
+// function PlayerService(){
+//     var _players = [
+//     {name: 'Player Name',
+//     position: 'Player Position',
+//     jersey: 'Player Number',
+//     id: 1
+// },{
+//     name: 'Meredith',
+//     position: 'Playa',
+//     jersey: 1,
+//     id: 2
+// },{
+//     name: 'Goofy',
+//     position: 'QB',
+//     jersey: 2,
+//     id = 3,
+// },]
 
 
-function NewPlayer(name,position,jersey){
-    this.name = name;
-    this.position = position;
-    this.jersey = jersey;
-}
+// function Player(name,position,jersey){
+//     this.name = name;
+//     this.position = position;
+//     this.jersey = jersey;
+//     this.id= id;
+//     id++;
+// }
 
-var players = [
-    {name: 'Player Name',
-    position: 'Player Position',
-    jersey: 'Player Number'},
-]
+// this.getPlayers = function(){
+//     return _players
+// }
 
-$("form").submit(function(event){
-     event.preventDefault();
-     var form = this;
+// this.addPlayer = function(name, position, jersey){
+//     if(!name || !pos|| !jersey){
+//         return
+//     }
+//     var player = new Player(name, position, jersey)
+//     _players.push(player)
+
+// }
+
+// this.removePlayer = function(id){
+//     console.log(id)
+//     for(var i =0; 0 < _players.length; i++){
+//         if(_players[i].id == id){
+//         _players.splice(i,1)
+//         return
+//         }
+//     }
+// }
+// }
+
+// // ENDED HERE 
+
+
+// $("form").submit(function(event){
+//      event.preventDefault();
+//      var form = this;
     
-    var p = new NewPlayer(
-        form.inputName.value,
-        form.inputPosition.value,
-        form.inputNumber.value
-    )
+//     var p = new NewPlayer(
+//         form.inputName.value,
+//         form.inputPosition.value,
+//         form.inputNumber.value
+//     )
 
-    console.log(p)
+//     console.log(p)
 
-players.push(p);
-form.reset();
-cards(players);
-});
+// players.push(p);
+// form.reset();
+// cards(players);
+// });
 
-function cards(players){
-    var elem = document.getElementById('player-roster')
-    elem.innerHTML = ''
-    var template = ''
-    for(var i=0; i<players.length; i++){
-        var nfl= players[i]
-        template+= `
-        <div class="card">
-        <div class="player-card">
-         <img class="card-img-top" src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/">
-         <div class="card-block">
-          <p class="card-text cName text">${nfl.name}</p>
-         <p class="card-text cPosition text">${nfl.position}</p>
-         <p class="card-text cNumber text">${nfl.jersey}</p>
-         </div>
-         </div>
-         </div>`
-    }
+// function cards(players){
+//     var elem = document.getElementById('player-roster')
+//     elem.innerHTML = ''
+//     var template = ''
+//     for(var i=0; i<players.length; i++){
+//         var nfl= players[i]
+//         template+= `
+//         <div class="card">
+//         <div class="player-card">
+//          <img class="card-img-top" src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/">
+//          <div class="card-block">
+//           <p class="card-text cName text">${nfl.name}</p>
+//          <p class="card-text cPosition text">${nfl.position}</p>
+//          <p class="card-text cNumber text">${nfl.jersey}</p>
+//          </div>
+//          </div>
+//          </div>`
+//     }
 
-    elem.innerHTML = template;
-}
+//     elem.innerHTML = template;
+// }
 
-cards(players);
+// cards(players);
 
 
 
---
+++ service.js
@@ -0,0 +1,71 @@
+function PlayerService(){
+    var _players = [];
+    var _playersData = [];
+
+    function Player(name, position, jersey){
+        this.name = name;
+        this.position = position;
+        this.jersey= jersey;
+        this.id = id;
+        id ++;
+    }
+
+this.getPlayersByTeam = function(teamName){
+    _playersData.filter(function(player){
+        if(player.team == teamName){
+            return true;
+        });
+}    
+
+this.getPlayersByPositions = function(position){
+_playersData.filter(function(player){
+    if(player.position == position){
+        return true;
+    }
+});
+}
+
+
+this.getPlayers = function(){
+    return _players
+}
+
+this.getPlayers = function(){
+    if(!name || ![position|| !jersey]){
+        return
+    }
+    var player = new Player(name, position, jersey)
+    _players.push(player)
+}
+
+this.removePlayer = function(id){
+    for(var i=0; i<_players.length; i++){
+        if(player.id == id){
+            return _players.splice(i,1)
+        }
+    }
+}
+
+ this.getNFL = function loadPlayersData(callback){
+      var apiUrl = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
+
+       var localData = localStorage.getItem('playersData');
+      if(localData){
+        playersData = JSON.parse(localData);
+        return callback(playersData); 
+      }
+
+            var url = "http://bcw-getter.herokuapp.com/?url=";
+      var endPointUrl = url + encodeURIComponent(apiUrl);
+        $.getJSON(endPointUrl, function(data){
+          playersData = data.body.players;
+          console.log('Player Data Ready')
+          console.log('Writing Player Data to localStorage')
+          localStorage.setItem('playersData', JSON.stringify(playersData))
+          console.log('Finished Writing Player Data to localStorage')
+          callback(playersData)
+        });
+    }   
+loadPlayersData();
+}
+
